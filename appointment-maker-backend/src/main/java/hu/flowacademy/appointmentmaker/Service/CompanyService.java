fd66db25b8b9e17a1bccc737eedb616fd22dd83e
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-29 14:27:20 +0200

    Made crud for companies When we wilÃl have individual exceptions then must change as comment say in the code.

--- appointment-maker-backend/src/main/java/hu/flowacademy/appointmentmaker/Service/CompanyService.java
+++ appointment-maker-backend/src/main/java/hu/flowacademy/appointmentmaker/Service/CompanyService.java
@@ -1,4 +1,42 @@
 package hu.flowacademy.appointmentmaker.Service;
 
+import hu.flowacademy.appointmentmaker.Model.Company;
+import hu.flowacademy.appointmentmaker.Repository.CompanyRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@Transactional
 public class CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public Company createCompany(Company company) {
+        return companyRepository.save(company);
+    }
+    /*
+    Change the fondOneCompany to this code when the individual exceptions are going to be done.
+
+     public Optional<Company> fondOneCompany(Long id) {
+        return Optional.of(companyRepository.findById(id))
+                .orElseThrow(() -> new CompanyNotFoundException(id));
+     }*/
+
+    public Company findOneCompany(Long id) {
+        return companyRepository.findById(id).get();
+    }
+
+    public Company updateCompanyDetails(Long id, Company company) {
+        company.setId(id);
+        return companyRepository.save(company);
+    }
+
+    public void deleteCompany(Long id) {
+        companyRepository.deleteById(id);
+    }
 }
