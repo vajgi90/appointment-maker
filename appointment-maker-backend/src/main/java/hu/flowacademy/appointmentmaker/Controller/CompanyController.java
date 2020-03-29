842e0df197fbda91f5e3900bf25663a4a038f311
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-29 14:42:12 +0200

    made the controller for  the company model.

--- appointment-maker-backend/src/main/java/hu/flowacademy/appointmentmaker/Controller/CompanyController.java
+++ appointment-maker-backend/src/main/java/hu/flowacademy/appointmentmaker/Controller/CompanyController.java
@@ -1,4 +1,48 @@
 package hu.flowacademy.appointmentmaker.Controller;
 
+import hu.flowacademy.appointmentmaker.Model.Company;
+import hu.flowacademy.appointmentmaker.Service.CompanyService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/companies")
+@AllArgsConstructor
 public class CompanyController {
+
+    private CompanyService companyService;
+
+    @PostMapping
+    public ResponseEntity<Company> createCompany(@RequestBody Company company) {
+        return new ResponseEntity<>(companyService.createCompany(company), HttpStatus.OK);
+    }
+
+    /*
+    When the service start to use individual exceptions then we gonna use this findOne.
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> findOne(@PathVariable Long id) {
+        return companyService.findOne(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+     */
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> findOne(@PathVariable Long id) {
+       return new ResponseEntity<>(companyService.findOneCompany(id), HttpStatus.FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Company> updateCompany (@PathVariable Long id, @RequestBody Company company){
+        companyService.updateCompanyDetails(id, company);
+        return ResponseEntity.accepted().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable long id) {
+        companyService.deleteCompany(id);
+        return ResponseEntity.noContent().build();
+    }
 }
