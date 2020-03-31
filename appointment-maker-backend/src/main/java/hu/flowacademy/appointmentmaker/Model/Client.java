97cc4399dc516c6af44526b2305a59dcabba9e63
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-31 11:41:13 +0200

    Client Entity has been created.

--- appointment-maker-backend/src/main/java/hu/flowacademy/appointmentmaker/Model/Client.java
+++ appointment-maker-backend/src/main/java/hu/flowacademy/appointmentmaker/Model/Client.java
@@ -1,4 +1,38 @@
 package hu.flowacademy.appointmentmaker.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Transient
+    @JsonIgnore
+    private String password;
+
+    @Column
+    private Integer phoneNumber;
+
+    @Column(unique = true, length = 100)
+    private String email;
+
+    @ManyToOne
+    private Company companies;
 }
